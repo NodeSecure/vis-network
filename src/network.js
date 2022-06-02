d2ab2ec966e3df2f87a493ab3d46e8972e43cb77
MehdiHan <meh.bouchard@outlook.fr>
2022-06-02 15:26:53 +0200

    upstream

--- src/network.js
+++ src/network.js
@@ -58,6 +58,9 @@ export default class NodeSecureNetwork {
 
   /**
    * @param {!NodeSecureDataSet} secureDataSet
+   * @param {object} [options]
+   * @param {"LIGHT" | "DARK"} [options.theme="LIGHT"]
+   * @param {*} [options.colors]
    */
   constructor(secureDataSet, options = {}) {
     console.log("[Network] created");
@@ -74,7 +77,11 @@ export default class NodeSecureNetwork {
     if(!(theme in CONSTANTS.COLORS)){
       throw new Error(`Unknown theme ${options.theme}. Theme value can be LIGHT or DARK`)
     }
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> 24291ad4522901de57612474a3be7b2352212709
     this.theme = theme;
     this.colors = { ...CONSTANTS.COLORS[this.theme], ...(options.colors ?? {}) };
 
