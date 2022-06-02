ca58e3694a58404b04927d0520504def66b1a2b5
MehdiHan <meh.bouchard@outlook.fr>
2022-06-02 15:28:41 +0200

    chore: update color scheme

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 export const COLORS = Object.freeze({
   LIGHT: {
-<<<<<<< HEAD
     MAIN: "#01579B", 
     INDIRECT: "rgba(107, 178, 178, 1)", 
     WARN: "rgba(210, 115, 115, 1)", 
@@ -15,21 +14,5 @@ export const COLORS = Object.freeze({
     NORMAL: "rgba(150, 200, 200, 0.15)", 
     SELECTED: "rgba(170, 100, 200, 0.50)", 
     TRS: "rgba(150, 150, 150, 0.02)", 
-=======
-    MAIN: "#01579B",
-    INDIRECT: "rgba(100, 200, 200, 1)",
-    WARN: "rgba(210, 115, 115, 1)",
-    NORMAL: "rgba(150, 200, 200, 1)",
-    SELECTED: "rgba(170, 100, 200, 1)",
-    TRS: "rgba(150, 150, 150, 1)",
-  },
-  DARK: {
-    MAIN: "#01579B",
-    INDIRECT: "rgba(63, 214, 214, 1)",
-    WARN: "rgba(241, 69, 69, 1)",
-    NORMAL: "rgba(75, 231, 231, 1)",
-    SELECTED: "rgba(157, 66, 197, 1)",
-    TRS: "rgba(255, 255, 255, 1) ",
->>>>>>> 24291ad4522901de57612474a3be7b2352212709
   },
 });
