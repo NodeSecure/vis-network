5667b79e5a149059988e97ddb676b89dd4f03347
PierreDemailly <pierredemailly.pro@gmail.com>
2022-07-11 11:42:27 +0200

    chore: remove useless assertions

--- test/dataset.test.js
+++ test/dataset.test.js
@@ -12,18 +12,6 @@ tap.test("NodeSecureDataSet.init with given payload", async(t) => {
   await nsDataSet.init(dataSetPayload);
 
   t.equal(nsDataSet.data, dataSetPayload, "should set data");
-  t.equal(nsDataSet.warnings.length, 2, "should have 2 warnings");
-  t.equal(nsDataSet.dependenciesCount, 3, "should have 3 dependencies");
-  t.equal(nsDataSet.indirectDependencies, 1, "should have 1 indirect dependency");
-  t.equal(nsDataSet.size, 400, "should have 400 bytes");
-  t.equal(nsDataSet.packages.length, 3, "should have 3 packages");
-  t.equal(nsDataSet.linker.size, 3, "should have 3 linker entries");
-  t.equal(nsDataSet.rawNodesData.length, 3, "should have 3 raw nodes");
-  t.equal(nsDataSet.rawEdgesData.length, 2, "should have 2 raw edges");
-  t.equal(nsDataSet.authors.size, 1, "should have 1 author");
-  t.equal(Object.keys(nsDataSet.extensions).length, 5, "should have 5 extensions");
-  t.equal(Object.keys(nsDataSet.licenses).length, 3, "should have 3 licenses");
-  t.equal(nsDataSet.licenses.Unknown, 1, "should have 1 unknown license");
 
   t.end();
 });
